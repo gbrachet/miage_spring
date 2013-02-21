1676b78ad85a599acbe58bc3d1a378057c2fce25
BRACHET Gautier <g.brachet@gmail.com>
2013-02-21 23:11:02 +0100

    Webservice

--- ProjetSpring/src/main/java/service/MessageService.java
+++ ProjetSpring/src/main/java/service/MessageService.java
@@ -2,11 +2,17 @@ package service;
 
 import java.util.List;
 
+import javax.jws.WebParam;
+import javax.jws.WebResult;
+import javax.jws.WebService;
+
 import domain.Message;
 
+@WebService
 public interface MessageService {
 
-	public List<Message> getMessages(long id);
+	@WebResult(name = "getMessages")
+	public List<Message> getMessages(@WebParam(name="id") long id);
 	
 	public void addMessage(String message, long from, long to);
 }
